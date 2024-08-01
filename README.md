8256b62840e87135bf789043ec297057450dfaed
Zhao Wei Liew <zhaoweiliew@gmail.com>
2024-08-01 23:28:36 +0800

    Clean up code

--- README.md
+++ README.md
@@ -14,5 +14,5 @@ A simple CLI wrapper for GPT.
 ## Usage
 
 ```sh
-$ jippity [-c] [-s "<system_message>"] "<my_prompt_that_i_urgently_hope_my_ai_overlords_can_answer_satisfactorily>"
+$ jippity [-s "<system_message>"] [-c] "<my_prompt_that_i_urgently_hope_my_ai_overlords_can_answer_satisfactorily>"]
 ```
--- bin/main.ml
+++ bin/main.ml
@@ -58,6 +58,15 @@ type message_log_item =
 
 type message_log = message_log_item list [@@deriving yojson]
 
+let log_file_path =
+  let home_path =
+    match Sys.getenv_opt "HOME" with
+    | Some x -> x
+    | None -> failwith "Unable to find HOME"
+  in
+  Filename.concat home_path "jippity_last_convo.json"
+;;
+
 let parse_top_choice resp =
   let resp = Yojson.Safe.from_string resp in
   let resp = response_of_yojson resp in
@@ -78,8 +87,7 @@ let https ~authenticator =
     Tls_eio.client_of_flow ?host tls_config raw
 ;;
 
-let () =
-  (* Parse cmdline args *)
+let parse_args () =
   let prompt = ref "" in
   let sys_msg = ref "" in
   let continue = ref false in
@@ -93,57 +101,50 @@ let () =
   (* Check for arg errors *)
   let prompt =
     match !prompt with
-    | "" -> failwith "Missing prompt."
+    | "" -> invalid_arg "Missing prompt."
     | x -> x
   in
   let sys_msg = String.trim !sys_msg in
   let continue = !continue in
   if sys_msg <> "" && continue
-  then failwith "Cannot specify system message in a continued conversation.";
-  let safe_sys_msg =
+  then invalid_arg "Cannot specify system message in a continued conversation.";
+  let sys_msgs =
     match sys_msg with
-    | "" -> ""
-    | x ->
-      Printf.sprintf {|{ "role": "system", "content": %s }|}
-      @@ Yojson.Safe.to_string
-      @@ `String x
+    | "" -> []
+    | x -> [ { role = "system"; content = x } ]
   in
-  (* Read API key *)
+  prompt, sys_msgs, continue
+;;
+
+let read_api_key () =
   Dotenv.export ();
-  let api_key =
-    match Sys.getenv_opt "OPENAI_API_KEY" with
-    | Some x -> x
-    | None -> failwith "API_KEY not found in environment variables."
-  in
-  (* Construct and send request *)
-  let log_file_path = Filename.concat (Unix.getenv "HOME") "jippity_last_convo.json" in
+  let api_key = Sys.getenv_opt "OPENAI_API_KEY" in
+  match api_key with
+  | Some x -> x
+  | None -> failwith "OPENAI_API_KEY not found in environment variables."
+;;
+
+let init_msg_log prompt sys_msgs continue =
   let open_flags = [ Open_rdonly; Open_creat; Open_text ] in
   let open_flags = if continue then open_flags else Open_trunc :: open_flags in
-  let msg_log =
-    In_channel.with_open_gen open_flags 0o644 log_file_path (fun ic ->
-      (match In_channel.input_all ic with
-       | "" -> Printf.sprintf "[%s]" safe_sys_msg
-       | x -> x)
-      |> Yojson.Safe.from_string
-      |> message_log_of_yojson)
+  let read_log_file default ic =
+    (match In_channel.input_all ic with
+     | "" -> Yojson.Safe.to_string (yojson_of_message_log default)
+     | x -> x)
+    |> Yojson.Safe.from_string
+    |> message_log_of_yojson
   in
-  let open_flags = [ Open_wronly; Open_trunc; Open_creat; Open_text ] in
-  Eio_main.run
-  @@ fun env ->
-  Mirage_crypto_rng_eio.run (module Mirage_crypto_rng.Fortuna) env
-  @@ fun () ->
-  let client =
-    Cohttp_eio.Client.make ~https:(Some (https ~authenticator:null_auth)) env#net
-  in
-  Eio.Switch.run
-  @@ fun sw ->
+  In_channel.with_open_gen open_flags 0o644 log_file_path (read_log_file sys_msgs)
+  @ [ { role = "user"; content = prompt } ]
+;;
+
+let send_req client sw msg_log api_key =
   let headers = Cohttp.Header.init_with "Content-Type" "application/json" in
   let headers = Cohttp.Header.add headers "Authorization" ("Bearer " ^ api_key) in
-  let msg_log = msg_log @ [ { role = "user"; content = prompt } ] in
   let req =
     Printf.sprintf
       {|{ "model": "gpt-4o-mini", "messages": %s }|}
-      (Yojson.Safe.to_string @@ yojson_of_message_log msg_log)
+      (Yojson.Safe.to_string (yojson_of_message_log msg_log))
   in
   let body = Eio.Flow.string_source req in
   let resp, body =
@@ -154,13 +155,32 @@ let () =
       client
       (Uri.of_string "https://api.openai.com/v1/chat/completions")
   in
-  (* Print response and log to file *)
   if Http.Status.compare resp.status `OK <> 0
   then Fmt.epr "Unexpected HTTP status: %a" Http.Status.pp resp.status;
-  let body = Eio.Buf_read.(parse_exn take_all) body ~max_size:max_int in
+  Eio.Buf_read.(parse_exn take_all) body ~max_size:max_int
+;;
+
+let write_msg_log msg_log =
+  let open_flags = [ Open_wronly; Open_trunc; Open_creat; Open_text ] in
+  Out_channel.with_open_gen open_flags 0o644 log_file_path (fun oc ->
+    Out_channel.output_string oc (Yojson.Safe.to_string (yojson_of_message_log msg_log)))
+;;
+
+let () =
+  let prompt, sys_msgs, continue = parse_args () in
+  let api_key = read_api_key () in
+  let msg_log = init_msg_log prompt sys_msgs continue in
+  Eio_main.run
+  @@ fun env ->
+  Mirage_crypto_rng_eio.run (module Mirage_crypto_rng.Fortuna) env
+  @@ fun () ->
+  Eio.Switch.run
+  @@ fun sw ->
+  let client =
+    Cohttp_eio.Client.make ~https:(Some (https ~authenticator:null_auth)) env#net
+  in
+  let body = send_req client sw msg_log api_key in
   let reply_msg = parse_top_choice body in
   print_endline reply_msg;
-  let msg_log = msg_log @ [ { role = "assistant"; content = reply_msg } ] in
-  Out_channel.with_open_gen open_flags 0o644 log_file_path (fun oc ->
-    Out_channel.output_string oc @@ Yojson.Safe.to_string @@ yojson_of_message_log msg_log)
+  write_msg_log (msg_log @ [ { role = "assistant"; content = reply_msg } ])
 ;;
